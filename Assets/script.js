0032c8d9637cebfb311739fd4216da7ef1b5c1d4
Sam Higa <samhiga@Sams-MacBook-Pro.local>
2023-04-06 21:34:36 -0700

    added button that saves user input through local storage and also added a var to save and retrive from local storage, also added function for rows to change color based on the time, added localStorage.getItem for each hour, added pseudo code explaining in Js file what everything does

--- Assets/script.js
+++ Assets/script.js
@@ -1,32 +1,48 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-
-
-
-// $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+// this function displays the current time and date
   $(document).ready(function (){
 setInterval(function(){
 $("#currentDay").text(dayjs().format("dddd, MMMM D YYYY, h:mm:ss a"));
-}, 1000);
+}, )
+// this function allows me to save to local storage whatever is written in the rows by clicking the button
+$(".saveBtn").on("click", function(){
+  var userInput = $(this).siblings('.description').val()
+  var timeBlock = $(this).parent().attr('id')
+  localStorage.setItem(timeBlock, userInput)
+})
+// allows for anything written down to stay on the page after being refreshed  
+$('#hour-09 .description').val(localStorage.getItem("hour-09"))
+$('#hour-10 .description').val(localStorage.getItem("hour-10"))
+$('#hour-11 .description').val(localStorage.getItem("hour-11"))
+$('#hour-12 .description').val(localStorage.getItem("hour-12"))
+$('#hour-13 .description').val(localStorage.getItem("hour-13"))
+$('#hour-14 .description').val(localStorage.getItem("hour-14"))
+$('#hour-15 .description').val(localStorage.getItem("hour-15"))
+$('#hour-16 .description').val(localStorage.getItem("hour-16"))
+$('#hour-17 .description').val(localStorage.getItem("hour-17"))
+
+// function that allows the color of the rows to change according to the time of day, -2 allows me to target only the last 2 characters of the hour id(the numbers in this case)
+function currentColor(){
+  var currentHour = dayjs().hour()
+  $('.time-block').each(function() {
+    var currentId = $(this).attr('id')
+    var numberHour = currentId.slice(-2)
+    if (numberHour < currentHour) {
+      $(this).removeClass('present')
+      $(this).removeClass('future')
+      $(this).addClass('past')
+    } else if(currentHour == numberHour){
+      $(this).removeClass('past')
+      $(this).removeClass('future')
+      $(this).addClass('present')
+    }else if (numberHour > currentHour){
+      $(this).removeClass('past')
+      $(this).removeClass('present')
+      $(this).addClass('future')
+    }
+  })
+}
+
+currentColor()
+
 });
-// });
+
